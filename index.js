e62b8795a84ac74f7babafd1b47edea40c17bbea
perttunurmi <perttu.nurmi@gmail.com>
2024-12-12 22:17:02 +0200

    3.7-3.8

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express")
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+morgan.token('body', request => {
+    if (request.method == "POST") {
+        return JSON.stringify(request.body)
+    } else {
+        return " "
+    }
+})
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 let persons = [
     {
