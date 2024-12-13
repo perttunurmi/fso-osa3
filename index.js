8bca6216176454ae2a3fe43645eaa9f1df67b822
perttunurmi <perttu.nurmi@gmail.com>
2024-12-13 21:29:53 +0200

    dist

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(express.static('dist'))
 
 app.use(express.json())
+app.use(cors())
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
