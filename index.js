d2d23cc7535f5fcb24e3d7e51170ea38f2db77b3
perttunurmi <perttu.nurmi@gmail.com>
2025-03-21 16:43:08 +0200

    ilmeisesti muutosta pelkästään muotoiluun

--- index.js
+++ index.js
@@ -1,94 +1,97 @@
-const express = require("express")
-const morgan = require('morgan')
-const app = express()
-app.use(express.json())
-morgan.token('body', request => {
-    if (request.method == "POST") {
-        return JSON.stringify(request.body)
-    } else {
-        return " "
-    }
-})
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+app.use(express.json());
+morgan.token("body", (request) => {
+  if (request.method == "POST") {
+    return JSON.stringify(request.body);
+  } else {
+    return " ";
+  }
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "39-23-6523122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "39-23-6523122",
+  },
+];
 
 app.get("/api/persons", (_request, response) => {
-    response.json(persons)
-})
+  response.json(persons);
+});
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
 
-    if (person) {
-        return response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
+  if (person) {
+    return response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
 
 app.delete("/api/persons/:id", (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
 
-    response.status(204).end()
-})
+  response.status(204).end();
+});
 
 app.post("/api/persons", (request, response) => {
-    const body = request.body
+  const body = request.body;
 
-    if (!body.name) {
-        return response.status(400).json({
-            error: "name is required"
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({
-            error: "number is required"
-        })
-    }
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name is required",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number is required",
+    });
+  }
 
-    if (persons.find((person) => person.name === body.name) !== undefined) {
-        return response.status(400).json({
-            error: "name must be unique"
-        })
-    }
+  if (persons.find((person) => person.name === body.name) !== undefined) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * Math.pow(10, 10)),
-    }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * Math.pow(10, 10)),
+  };
 
-    persons = persons.concat(person)
+  persons = persons.concat(person);
 
-    response.json(person)
-})
+  response.json(person);
+});
 
 app.get("/info", (_request, response) => {
-    const date = new Date()
-    response.send(`<div><div>Phonebook has info for ${persons.length} people</div><div>${date}</div></div>`)
-})
+  const date = new Date();
+  response.send(
+    `<div><div>Phonebook has info for ${persons.length} people</div><div>${date}</div></div>`,
+  );
+});
 
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
