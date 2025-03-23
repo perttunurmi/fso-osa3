037b786214cb41c0eec5ee1f80c458d24f3af9cb
perttunurmi <perttu.nurmi@gmail.com>
2025-03-23 14:21:00 +0200

    deleting added numbers should work

--- backend/index.js
+++ backend/index.js
@@ -48,7 +48,7 @@ app.get("/api/persons/:id", (request, response) => {
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
+  const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
