1bfd0c90de111e352f42fcf092ea86bfeacfafeb
perttunurmi <perttu.nurmi@gmail.com>
2025-06-14 21:04:26 +0000

    esimerkki

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const app = express();
-app.use(express.static("dist"));
 require("dotenv").config();
 const Note = require("./models/note");
-
-app.use(express.json());
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -14,6 +11,8 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(express.static("dist"));
+app.use(express.json());
 app.use(requestLogger);
 
 let notes = [
@@ -44,17 +43,26 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-
-  response.status(204).end();
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateId = () => {
@@ -82,11 +90,42 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
+
+  NotefindById(request.params.id)
+    .then((note) => {
+      if (!note) {
+        return response.status(404).end();
+      }
+
+      note.content = content;
+      note.important = important;
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote);
+      });
+    })
+    .catch((error) => next(error));
+});
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler); // tämä tulee kaikkien muiden middlewarejen ja routejen rekisteröinnin jälkeen!
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
